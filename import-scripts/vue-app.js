23e0e7d8ec35d131994367847564d50729fb15d5
Danny Mecca <mecca2@clemson.edu>
2018-09-06 18:16:28 -0400

    Cleaned up formatting.

--- import-scripts/vue-app.js
+++ import-scripts/vue-app.js
@@ -1,74 +1,80 @@
-
-
 const vm = new Vue({
   el: '.vue-app',
   data: {
-    results: [], 
-    hourly_rate: 50, 
+    results: [],
+    hourly_rate: 50,
     single_item: {
-      name: '', 
-      low_hours: '', 
-      high_hours: ''
-    }, 
-    total_low_hours: 0, 
-    total_high_hours: 0, 
+      name: '',
+      low_hours: '',
+      high_hours: '', 
+      active: false
+    },
+    total_low_hours: 0,
+    total_high_hours: 0,
     error: false,
-    error_message:''
+    error_message: ''
   },
   mounted() {
-   this.item = Object.assign({}, this.single_item); 
-   if (localStorage.getItem('results')){
-    this.results = JSON.parse(localStorage.getItem('results'));
-   }
-  }, 
+    this.item = Object.assign({}, this.single_item);
+    if (localStorage.getItem('results')) {
+      this.results = JSON.parse(localStorage.getItem('results'));
+    }
+  },
   computed: {
-    calculateLowHours: function(){
-      var total = 0 ; 
-      this.results.forEach(function(element){
-        total += Number(element.low_hours); 
-      }); 
-      this.total_low_hours = total; 
-      return total; 
+    calculateLowHours: function() {
+      var total = 0;
+      this.results.forEach(function(element) {
+        total += Number(element.low_hours);
+      });
+      this.total_low_hours = total;
+      return total;
     },
-    calculateHighHours: function(){
-      var total = 0 ; 
-      this.results.forEach(function(element){
-        total += Number(element.high_hours); 
-      }); 
-      this.total_high_hours = total; 
-      return total; 
+    calculateHighHours: function() {
+      var total = 0;
+      this.results.forEach(function(element) {
+        total += Number(element.high_hours);
+      });
+      this.total_high_hours = total;
+      return total;
     },
-    calculateMidHours: function(){
-      return (this.total_high_hours + this.total_low_hours) / 2; 
-    }, 
-    calculateCost: function(){
-      return this.calculateMidHours * this.hourly_rate; 
+    calculateMidHours: function() {
+      return (this.total_high_hours + this.total_low_hours) / 2;
+    },
+    calculateCost: function() {
+      return this.calculateMidHours * this.hourly_rate;
     }
 
-  }, 
-  methods : {
-    addItem : function(){
-     // console.log(this.single_item); 
-     this.error = false; 
-     var missing_fields = []; 
-     for(var key in this.single_item){
-      if(this.single_item[key] == ''){
-        this.error = true; 
-        missing_fields.push(key); 
+  },
+  methods: {
+    addItem: function() {
+      this.error = false;
+      var missing_fields = [];
+      console.log( this.single_item); 
+      for (var key in this.single_item) {
+        if (this.single_item[key] == '' && key != "active" ) {
+          this.error = true;
+          missing_fields.push(key);
+        }
       }
-     }
-     if(this.error){
-      this.error_message = 'The following fields are required: ' + missing_fields.toString(); 
-     }else{
-      this.results.push(this.single_item);
-      this.single_item = Object.assign({}, this.item)
-     }
-      
+      if (this.error) {
+        this.error_message = 'The following fields are required: ' + missing_fields.toString();
+      } else {
+        this.results.push(this.single_item);
+        this.single_item = Object.assign({}, this.item)
+      }
+
+    },
+    mouseOver: function(item) {
+     // console.log(item); 
+      console.log('test');
+      item.active = true;  
+      console.log(item); 
+
     }
-  }, 
+  },
   watch: {
     results: {
-      handler(){
+      handler() {
         localStorage.setItem('results', JSON.stringify(this.results));
       },
       deep: true,
